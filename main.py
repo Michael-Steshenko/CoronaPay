354f6e221f277141014fa14eb29cf54f56e0bff7
Michael Steshenko <oblivic@hotmail.com>
2020-03-30 15:17:21 +0300

    function to create a Paypal payment request URL

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import qrcode
 # generate a paypal payment link to the user identified by "email" for "sum" amount
 def CreatePayPalLink(sum, email):
     BaseUrl = "https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=EMAIL&amount=SUM&currency_code=ILS&item_name=test"
-    print(5)
     return BaseUrl.replace("SUM", sum).replace("EMAIL", email)
 
 def CreateQRPayCode(sum, email):
@@ -19,7 +18,6 @@ def CreateQRPayCode(sum, email):
     )
     # The data that you want to store
     data = CreatePayPalLink(sum, email)
-    print(6)
     # Add data
     qr.add_data(data)
     qr.make(fit=True)
@@ -34,6 +32,6 @@ def CreateQRPayCode(sum, email):
     img.save("image.jpg")
 
 
-CreateQRPayCode("20", "oblivic90@gmail.com")
+#CreateQRPayCode("20", "oblivic90@gmail.com")
 
 #print(CreatePayPalLink("20", "oblivic90@gmail.com"))
