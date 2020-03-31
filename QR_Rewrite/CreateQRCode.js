7ad1d4c70daf25ee15bd263ee91e88d44e356c11
Bobopopo <bensc1990@gmail.com>
2020-03-31 21:03:09 +0300

    Email changed, and currency code parameter has been added.

--- QR_Rewrite/CreateQRCode.js
+++ QR_Rewrite/CreateQRCode.js
@@ -1,19 +1,19 @@
-function CreateQRPayCode(sum, email){
+function CreateQRPayCode(sum, email) {
 
-	var qrcode = new QRCode("test", {
-	width: 200,
-	height: 200,
-	colorDark : "#000000",
-	colorLight : "#ffffff",
-	correctLevel : QRCode.CorrectLevel.H
-	});
-	
-	qrcode.makeCode(CreatePayPalLink(sum, email));
+    var qrcode = new QRCode("test", {
+        width: 200,
+        height: 200,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
+    });
 
-	function CreatePayPalLink(sum, email){
-		return ("https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business="+
-		email+"&amount="+sum+"&currency_code=ILS&item_name=test");
-	}
+    qrcode.makeCode(CreatePayPalLink(sum, email, currencyCode));
+
+    function CreatePayPalLink(sum, email) {
+        return ("https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=" +
+            email + "&amount=" + sum + "&currency_code=" + currencyCode + "&item_name=test");
+    }
 }
 
 
