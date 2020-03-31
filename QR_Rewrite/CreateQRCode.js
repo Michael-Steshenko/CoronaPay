3159bfbf7308ecc7eab6a4ccd2796b6bcf562704
Bobopopo <bensc1990@gmail.com>
2020-03-31 20:32:15 +0300

    QR Maker (Rewrite)

---
+++ QR_Rewrite/CreateQRCode.js
@@ -0,0 +1,19 @@
+function CreateQRPayCode(sum, email){
+
+	var qrcode = new QRCode("test", {
+	width: 200,
+	height: 200,
+	colorDark : "#000000",
+	colorLight : "#ffffff",
+	correctLevel : QRCode.CorrectLevel.H
+	});
+	
+	qrcode.makeCode(CreatePayPalLink(sum, email));
+
+	function CreatePayPalLink(sum, email){
+		return ("https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business="+
+		email+"&amount="+sum+"&currency_code=ILS&item_name=test");
+	}
+}
+
+
